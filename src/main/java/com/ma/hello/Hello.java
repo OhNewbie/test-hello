f302e277df01e5d393c0740e81379df710657665
ma-y-y <983534996@qq.com>
2021-09-09 09:30:51 +0800

    Initial commit

---
+++ src/main/java/com/ma/hello/Hello.java
@@ -0,0 +1,11 @@
+package com.ma.hello;
+
+/**
+ * @author ：Myy
+ * @date ：Created in 2021/9/9 9:28
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello github!");
+    }
+}
